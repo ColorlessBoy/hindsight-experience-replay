3806af827decaff47c987af6c69289a5d288623a
penglingwei <penglingwei@zju.edu.cn>
2020-09-09 17:07:07 +0800

    init gac

--- arguments.py
+++ arguments.py
@@ -8,6 +8,7 @@ Here are the param for the training
 def get_args():
     parser = argparse.ArgumentParser()
     # the environment setting
+    parser.add_argument('--alg', type=str, default='ddpg', help='the algorithm name')
     parser.add_argument('--env-name', type=str, default='FetchReach-v1', help='the environment name')
     parser.add_argument('--n-epochs', type=int, default=50, help='the number of epochs to train the agent')
     parser.add_argument('--n-cycles', type=int, default=50, help='the times to collect samples per epoch')
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import os, sys
 from arguments import get_args
 from mpi4py import MPI
 from rl_modules.ddpg_agent import ddpg_agent
+from rl_modules.gac_agent import gac_agent
 import random
 import torch
 
@@ -23,7 +24,6 @@ def get_env_params(env):
     return params
 
 def launch(args):
-    # create the ddpg_agent
     env = gym.make(args.env_name)
     # set random seeds for reproduce
     env.seed(args.seed + MPI.COMM_WORLD.Get_rank())
@@ -34,9 +34,18 @@ def launch(args):
         torch.cuda.manual_seed(args.seed + MPI.COMM_WORLD.Get_rank())
     # get the environment parameters
     env_params = get_env_params(env)
-    # create the ddpg agent to interact with the environment 
-    ddpg_trainer = ddpg_agent(args, env, env_params)
-    ddpg_trainer.learn()
+    
+    # create the ddpg_agent
+    if args.alg == 'gac':
+        # create the ddpg agent to interact with the environment 
+        printf("Using the generative policy.")
+        gac_trainer = gac_agent(args, env, env_params)
+        gac_trainer.learn()
+    else:
+        # create the ddpg agent to interact with the environment 
+        printf("Using the deterministic policy.")
+        ddpg_trainer = ddpg_agent(args, env, env_params)
+        ddpg_trainer.learn()
 
 if __name__ == '__main__':
     # take the configuration for the HER
